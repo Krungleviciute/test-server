8fbfe15f4e5ff1d2227a691f9f5cb85f61cfcc1f
Krungleviciute <45654453+Krungleviciute@users.noreply.github.com>
2024-06-20 14:21:32 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
           height="800px"
           name="myIframe"
           style={{ border: 'none' }}
-          src="https://demo.dev.secure.getborderless.com/widgets/add-external-contact?widgetId=Y2w5N2J4MXdjNTd0YTA4MzA4cnl4MXdncDphM2FiYjlhYS1iYzNkLTQ1YmQtOWRlNC01MjE2NjBkZTNmZTA="
+          src="https://demo.dev.secure.getborderless.com/widgets"
         />
       </div>
     </main>
